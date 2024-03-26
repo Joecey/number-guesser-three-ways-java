7ce98a6a4a4b6ff99adaf6410857ed4c90cc053c
Joe Linogao <linogaoj@tcd.ie>
2024-03-26 22:13:41 +0000

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Guess the Number Game Three Ways! (Java Edition) \n");
+
+        Scanner inputChooser = new Scanner(System.in);
+        System.out.println("Choose your version of Number Guesser");
+
+
+    }
+}
