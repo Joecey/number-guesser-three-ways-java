9e0b2bdc6dc93cb4063f8ee48dc32cda4b48d70f
Joe Linogao <linogaoj@tcd.ie>
2024-03-27 20:00:19 +0000

    Created string reverser in ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -2,6 +2,10 @@ public class ReverseString {
     private ReverseString(){}
 
     public static String createReverse(String input){
-        return input;
+        StringBuilder output = new StringBuilder();
+        for (int i = (input.length() - 1); i >= 0; i--){
+            output.append(input.charAt(i));
+        }
+        return output.toString();
     }
 }
